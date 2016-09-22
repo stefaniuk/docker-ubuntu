0f057cb389f463ec2b72c19956a30ee9b166fcb4
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2016-09-22 09:26:12 +0100

    Run custom scripts

--- assets/sbin/entrypoint.sh
+++ assets/sbin/entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
+# run bootstrap script
 [ -x /sbin/bootstrap.sh ] && . /sbin/bootstrap.sh "$@"
+
+# run custom scripts
+for file in /sbin/init.d/*; do
+    case "$file" in
+        *.sh)
+            echo "$0: running $file"
+            . $file
+            ;;
+        *)
+            echo "$0: ignoring $file"
+            ;;
+    esac
+    echo
+done
 
 exec gosu $SYSTEM_USER "$@"
