47f4fafbf5b8429089929e8e2b08c81f9c9f305d
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2017-06-19 14:54:34 +0200

    Fix init process issue

--- assets/sbin/entrypoint.sh
+++ assets/sbin/entrypoint.sh
@@ -10,11 +10,11 @@ if [[ "$INIT_GOSU" =~ ^(true|yes|on|1|TRUE|YES|ON)$ ]]; then
     [ -n "$INIT_RUN_AS" ] && gosu="gosu $INIT_RUN_AS" || gosu="gosu $SYSTEM_USER"
 fi
 
-# source init scripts
+# run init scripts
 for file in /sbin/init.d/*; do
     case "$file" in
         *.sh)
-            echo "$0: Running $file"
+            echo "$0: source $file"
             source $file "$@"
             ;;
     esac
