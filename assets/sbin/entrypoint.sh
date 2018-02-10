3f92c4cc868fd8f2b27846c3adadd39a33191fe8
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2018-02-10 22:29:51 +0000

    Do not produce any ourput on entrypoint

--- assets/sbin/entrypoint.sh
+++ assets/sbin/entrypoint.sh
@@ -14,11 +14,9 @@ fi
 for file in /sbin/init.d/*; do
     case "$file" in
         *.sh)
-            echo "$0: source $file"
             source $file "$@"
             ;;
     esac
-    echo
 done
 
 # run main process
